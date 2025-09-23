4f9e6d836592210e155dccccdcc38334f0b121d7
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-23 06:15:36 +0530

    __

---
+++ server.js
@@ -0,0 +1,33 @@
+const express=require('express');
+const http=require('http');
+const {Server}=require("socket.io");
+
+const app=express();
+const server=http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:8888",
+        methods:["GET","POST"],
+        credentials:true,
+    }
+});
+
+io.on('connection',(socket)=>{
+    const email=socket.handshake.query.email;
+    socket.join(email);
+    console.log("User connected:", email);
+
+    socket.on('send_message',(newChat)=>{
+        newChat.sender=email;
+        io.to(newChat.receiver).emit('receive_message',newChat);
+    });
+
+    socket.on('disconnect',()=>{
+        console.log("User disconnected:", email);
+    })
+});
+
+
+server.listen(4000,()=>{
+    console.log('Server running on port 4000');
+});
