85116efc21eee1b925c438216d08fa3abe54da2e
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-26 02:15:31 +0530

    __

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
         const cookies = cookie.parse(socket.handshake.headers.cookie || "");
         const token = cookies["hiUser"];
         //jwt.verify(token,secret);
-        io.to(socket.id).emit('error', cookies)
+        io.to(socket.id).emit('error', token)
         socket.on('send_message', (newChat) => {
             newChat.sender = email;
             io.to(newChat.receiver).emit('receive_message', newChat);
