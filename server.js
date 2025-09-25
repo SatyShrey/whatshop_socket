3e14c035bb7fe3624ea0fe832a4c34a8e26c3ed1
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-25 23:10:05 +0530

    __

--- server.js
+++ server.js
@@ -1,35 +1,51 @@
 require("dotenv").config()
-const express=require('express');
-const http=require('http');
-const {Server}=require("socket.io");
-
-const app=express();
-const server=http.createServer(app);
-const origin= process.env.origin
-const io = new Server(server,{
-    cors:{
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+const jwt= require('jsonwebtoken');
+const cookieParser=require('cookie-parser');
+const cookie = require("cookie");
+const app = express();
+app.use(cookieParser());
+const server = http.createServer(app);
+const origin = process.env.origin
+const secret = process.env.secret
+const io = new Server(server, {
+    cors: {
         origin,
-        methods:["GET","POST"],
-        credentials:true,
+        methods: ["GET", "POST"],
+        credentials: true,
     }
 });
 
-io.on('connection',(socket)=>{
-    const email=socket.handshake.query.email;
-    socket.join(email);
-    console.log("User connected:", email);
 
-    socket.on('send_message',(newChat)=>{
-        newChat.sender=email;
-        io.to(newChat.receiver).emit('receive_message',newChat);
+io.on('connection', (socket) => {
+   try{
+    const email = socket.handshake.query.email;
+    if(!email){return}
+    socket.join(email);
+    const cookies = cookie.parse(socket.handshake.headers.cookie || "");
+  const token = cookies["hiUser"];
+    jwt.verify(token,secret);
+    //console.log("User connected:", email);
+    socket.on('send_message', (newChat) => {
+        newChat.sender = email;
+        io.to(newChat.receiver).emit('receive_message', newChat);
+    });
+    
+    socket.on('send_otp', async(data) => {
+       const res= await sendMail(data);
+       io.to(data.email).emit('otp_sent', res);
     });
 
-    socket.on('disconnect',()=>{
-        console.log("User disconnected:", email);
+    socket.on('disconnect', () => {
+        //console.log("User disconnected:", email);
     })
+   }catch(err){io.to(socket.id).emit('error',err.message)}
 });
 
 
-server.listen(4000,()=>{
+
+server.listen(4000, () => {
     console.log('Server running on port 4000');
 });
