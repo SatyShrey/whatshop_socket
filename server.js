a178fa35916819f48ceacf36117c21337f9f1e43
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-26 02:03:09 +0530

    __

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("dotenv").config()
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
-const jwt= require('jsonwebtoken');
-const cookieParser=require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const cookie = require("cookie");
 const app = express();
 app.use(cookieParser());
@@ -20,28 +20,28 @@ const io = new Server(server, {
 
 
 io.on('connection', (socket) => {
-   try{
-    const email = socket.handshake.query.email;
-    if(!email){return}
-    socket.join(email);
-    const cookies = cookie.parse(socket.handshake.headers.cookie || "");
-  const token = cookies["hiUser"];
-    jwt.verify(token,secret);
-    //console.log("User connected:", email);
-    socket.on('send_message', (newChat) => {
-        newChat.sender = email;
-        io.to(newChat.receiver).emit('receive_message', newChat);
-    });
-    
-    socket.on('send_otp', async(data) => {
-       const res= await sendMail(data);
-       io.to(data.email).emit('otp_sent', res);
-    });
+    try {
+        const email = socket.handshake.query.email;
+        if (!email) { return }
+        socket.join(email);
+        const cookies = cookie.parse(socket.handshake.headers.cookie || "");
+        const token = cookies["hiUser"];
+        //jwt.verify(token,secret);
+        io.to(socket.id).emit('error', {cookies,token})
+        socket.on('send_message', (newChat) => {
+            newChat.sender = email;
+            io.to(newChat.receiver).emit('receive_message', newChat);
+        });
 
-    socket.on('disconnect', () => {
-        //console.log("User disconnected:", email);
-    })
-   }catch(err){io.to(socket.id).emit('error',err.message)}
+        socket.on('send_otp', async (data) => {
+            const res = await sendMail(data);
+            io.to(data.email).emit('otp_sent', res);
+        });
+
+        socket.on('disconnect', () => {
+            //console.log("User disconnected:", email);
+        })
+    } catch (err) { io.to(socket.id).emit('error', err.message) }
 });
 
 
